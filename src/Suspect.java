24ccb1b84caab540caea443abd71815d982ef5b4
Tamzid Khandoker <tamzidkhandoker@gmail.com>
2022-03-05 20:49:22 +0000

    created objects for each suspect

--- src/Suspect.java
+++ src/Suspect.java
@@ -1,6 +1,12 @@
 public class Suspect extends Person {
 
     public static final Suspect JON = new Suspect("Jon Lewis","Head of School", 800,500,"jon.png",false);
+    public static final Suspect OZ = new Suspect("Özgür Akgün","Lecturer", 800,500,"oz.png",false);
+    public static final Suspect RUTH = new Suspect("RUTH HOFFMAN","Lecturer", 800,500,"ruth.png",false);
+    public static final Suspect TRISTAN = new Suspect("Tristan Henderson","Lecturer", 800,500,"tristan.png",false);
+    public static final Suspect SALEEM = new Suspect("Saleem Bhatti","Professor", 800,500,"saleem.png",false);
+    public static final Suspect IAN = new Suspect("Ian Gent","Professor", 800,500,"ian.png",false);
+    public static final Suspect EDWIN = new Suspect("Edwin Brady","Reader", 800,500,"edwin.png",false);
 
     private boolean isMurderer;
 
