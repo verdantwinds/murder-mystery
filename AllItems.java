182739deeabc92d8e0e268096fde2484eaefcdae
Josh <jhdm1@st-andrews.ac.uk>
2022-03-05 20:12:45 +0000

    Progress

--- AllItems.java
+++ AllItems.java
@@ -1,18 +1,18 @@
 public class AllItems {
-    protected Item knife = new Item("Knife", "A sharp kitchen knife with a wooden handle", true);
-    protected Item switchBlade = new Item("Switch Blade", "A small pocket knife", true);
-    protected Item roundersBat = new Item("Rounders Bat", "A light wooden rounders bat", true);
-    protected Item pianoWire = new Item("Piano Wire", "Wire from a piano", true);
-    protected Item photoCopier = new Item("Photo Copier", "A machine that makes copies", false);
-    protected Item bag = new Item("Plastic Bag", "A small bag for life", true);
-    protected Item bleach = new Item("Bleach", "A bottle of toxic bleach", true);
-    protected Item screwdriver = new Item("Screwdrive", "A sharp small screwdriver", true);
-    protected Item corkScrew = new Item("Cork Screw", "A small sharp tool for opening bottles", true);
-    protected Item pen = new Item("Pen", "A black ball-point pen, true", true);
-    protected Item cereal = new Item("Cereal", "A box of Corn Flakes", true);
-    protected Item milk = new Item("Milk", "A carton of milk", true);
-    protected Item deskChair = new Item("Bleach", "A red desk chair", false);
-    protected Item cable = new Item("Cable", "The cable for a computer mouse", true);
+    protected Item knife = new Item("Knife", "A sharp kitchen knife with a wooden handle", "", true);
+    protected Item switchBlade = new Item("Switch Blade", "A small pocket knife", "", true);
+    protected Item roundersBat = new Item("Rounders Bat", "A light wooden rounders bat", "", true);
+    protected Item pianoWire = new Item("Piano Wire", "Wire from a piano", "",true);
+    protected Item photoCopier = new Item("Photo Copier", "A machine that makes copies", "",false);
+    protected Item bag = new Item("Plastic Bag", "A small bag for life", "", true);
+    protected Item bleach = new Item("Bleach", "A bottle of toxic bleach", "", true);
+    protected Item screwdriver = new Item("Screwdrive", "A sharp small screwdriver", "", true);
+    protected Item corkScrew = new Item("Cork Screw", "A small sharp tool for opening bottles", "", true);
+    protected Item pen = new Item("Pen", "A black ball-point pen, true", "", true);
+    protected Item cereal = new Item("Cereal", "A box of Corn Flakes", "", true);
+    protected Item milk = new Item("Milk", "A carton of milk", "", true);
+    protected Item deskChair = new Item("Bleach", "A red desk chair", "",false);
+    protected Item cable = new Item("Cable", "The cable for a computer mouse", "", true);
 
     public Item getKnife () {
         return knife;
