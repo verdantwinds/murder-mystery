2be5bacb69e5515a3c35318b45ca36fdc3832aeb
Tamzid Khandoker <tamzidkhandoker@gmail.com>
2022-03-06 04:45:41 +0000

    added murder weapon

--- src/Victim.java
+++ src/Victim.java
@@ -1,9 +1,12 @@
 public class Victim extends Person {
 
-    public Victim(String name, String description, int x, int y, String imageURL) {
+    private Weapon murderWeapon;
+
+    public Victim(String name, String description, int x, int y, String imageURL, Weapon murderWeapon) {
         super(name, description, x, y, imageURL);
+        this.murderWeapon = murderWeapon;
     }
 
-    public static final Victim Tamzid = new Victim("Tamzid Khandoker", "Poor sod who died", 400, 100, "assets/tamzid.png");
+    public static final Victim Tamzid = new Victim("Tamzid Khandoker", "Poor sod who died", 400, 100, "assets/tamzid.png", Weapon.MOUSE_CABLE);
 
 }
