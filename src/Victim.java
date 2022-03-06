ff97cd11032703a4d07dfad6b32ce113aff7817a
Tamzid Khandoker <tamzidkhandoker@gmail.com>
2022-03-06 04:19:34 +0000

    fixed victim

--- src/Victim.java
+++ src/Victim.java
@@ -1,5 +1,9 @@
 public class Victim extends Person {
 
-    public static final Victim = new Victim("Tamzid Khandoker", "Poor sod who died", 400, 100, "assets/tamzid.png");
-    
+    public Victim(String name, String description, int x, int y, String imageURL) {
+        super(name, description, x, y, imageURL);
+    }
+
+    public static final Victim Tamzid = new Victim("Tamzid Khandoker", "Poor sod who died", 400, 100, "assets/tamzid.png");
+
 }
